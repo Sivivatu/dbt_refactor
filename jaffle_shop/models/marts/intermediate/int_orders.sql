4a7cb5a8acbd9d3bd61b43c4e31552b9c672d02e
Paul <Sivivatu@users.noreply.github.com>
2024-10-22 14:35:46 +0100

    extract intermediate transforamtions

--- jaffle_shop/models/marts/intermediate/int_orders.sql
+++ jaffle_shop/models/marts/intermediate/int_orders.sql
@@ -23,6 +23,7 @@ paid_orders as (
     select
         orders.order_id,
         orders.customer_id,
+        orders.order_placed_at,
         orders.order_status,
         completed_payments.total_amount_paid,
         completed_payments.payment_finalized_date
